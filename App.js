948f67c018e5011f339f9c849746f2c3e10312ad
Kanji Hara <kanjih@ciandt.com>
2019-08-27 15:33:32 -0700

    Initial not completed version

--- App.js
+++ App.js
@@ -1,16 +1,96 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
+import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import EntryDetail from './components/EntryDetail'
+import Constants from 'expo-constants';
+import Live from './components/Live'
+
+
+
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+        <View style={{ flex: 1}}>
+          <AddEntry/>
+        </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1}}>
+        <History/>
+      </View>
+    );
+  }
+}
+
+class LiveScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1}}>
+        <Live/>
+      </View>
+    );
+  }
+}
+
+const Tabs = createBottomTabNavigator({
+  AddEntry: { screen: HomeScreen },
+  History: { screen: SettingsScreen },
+  Live: { screen: LiveScreen },
+});
+
+
+
+
+const MainNavigator = createAppContainer(createStackNavigator({
+  home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: ({ navigation }) => ({
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      },
+    }),
+  },
+}));
+
 
 export default class App extends React.Component {
   render() {
+    
+
     return (
       <Provider store={createStore(reducer)}>
-        <View>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <MainNavigator />
         </View>
       </Provider>
     )
